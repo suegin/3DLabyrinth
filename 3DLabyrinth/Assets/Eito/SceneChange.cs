525753da892a55e44a82467e11d800d8669fb63d
meronpanunn <fko2447021@stu.o-hara.ac.jp>
2024-10-01 19:05:05 +0900

    up

---
+++ 3DLabyrinth/Assets/Eito/SceneChange.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    
+    public void OnClick1()
+    {
+        SceneManager.LoadScene("Labyrinth Test");
+    }
+    //public void OnClick2()
+    //{
+    //    SceneManager.LoadScene("");
+    //}
+}
