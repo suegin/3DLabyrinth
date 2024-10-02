aa3c7135d13661f7e79d68d56b740abdafe02cd1
suegin <ginjiPG@gmail.com>
2024-10-02 18:22:45 +0900

    up

---
+++ 3DLabyrinth/Assets/Ginji/Ball.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("joystick button 0"))
+        {
+            Debug.Log("a");
+
+            Rigidbody rigidBody = GetComponent<Rigidbody>();
+            Vector3 ballForce = new Vector3(0.0f, 20.0f, 20.0f);
+            rigidBody.AddForce(ballForce, ForceMode.Impulse);
+        }
+    }
+}
