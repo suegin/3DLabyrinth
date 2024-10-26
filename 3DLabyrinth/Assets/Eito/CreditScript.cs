0e431f98f437b918ce381578755bed7180ce91e1
meronpanun <fko2447021@stu.o-hara.ac.jp>
2024-10-26 22:33:57 +0900

    up

--- 3DLabyrinth/Assets/Eito/CreditScript.cs
+++ 3DLabyrinth/Assets/Eito/CreditScript.cs
@@ -8,16 +8,21 @@ public class CreditScript : MonoBehaviour
 {
     private Vector3 staffrollPosition;
     private EventSystem m_eventSystem;
+    
     public RectTransform rectTransform; // スクロールしたいものを入れる
     public float endPos; // スクロールが終わる際のY座標の指定
     public GameObject creditView;
     public GameObject titleSceneView;
+
+    private AudioSource audioSource;
+    public AudioClip audioClip;
 
     // Start is called before the first frame update
     void Start()
     {
         staffrollPosition = rectTransform.anchoredPosition;
         m_eventSystem = EventSystem.current;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -36,7 +41,7 @@ public class CreditScript : MonoBehaviour
         // Bボタンを押すとタイトルシーンに戻る
         if (Input.GetKey("joystick button 1"))
         {
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(2); // クレジットリセット(タイトルシーン再ロード)
             creditView.SetActive(false);
             titleSceneView.SetActive(true);
             FocusButton();
@@ -45,7 +50,7 @@ public class CreditScript : MonoBehaviour
         // +Y座標にスクロールする
         if (rectTransform.anchoredPosition.y < endPos)
         {
-            staffrollPosition.y += 0.3f;
+            staffrollPosition.y += 0.3f; // スクロールスピード
             rectTransform.anchoredPosition = staffrollPosition;
         }
     }
