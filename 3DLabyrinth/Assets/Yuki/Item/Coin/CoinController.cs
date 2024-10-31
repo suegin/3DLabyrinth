b316d4de2e72608815a0a13273bc6e0158a13234
YukiCreative <166368392+YukiCreative@users.noreply.github.com>
2024-10-31 13:44:49 +0900

    エフェクト実装中

--- 3DLabyrinth/Assets/Yuki/Item/Coin/CoinController.cs
+++ 3DLabyrinth/Assets/Yuki/Item/Coin/CoinController.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class CoinController : MonoBehaviour
 {
     private Vector3 m_rotaSpeed = new Vector3(0.0f, 500.0f, 0.0f);
+    private static GameObject s_particle;
+
+    private void Start()
+    {
+        s_particle = (GameObject)Resources.Load("Basic Hit");
+    }
 
     // Update is called once per frame
     void Update()
@@ -27,6 +33,9 @@ public class CoinController : MonoBehaviour
         CoinSEController.Play();
         // コインを加算
         CoinNumberController.IncreaseCoin();
+
+        // パーティクル
+        Instantiate(s_particle, transform.position, Quaternion.identity);
 
         Destroy(gameObject);
     }
